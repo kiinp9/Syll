19e1abd3f68934e5d1eb75d971087f3a1ab3373f
nghiant <nghiant9608@gmail.com>
2025-11-10 10:14:56 +0700

    fe route gv chưa đăng nhập thì cho ra login

--- gv.syll.fe/src/routes/gv/(core)/+layout.svelte
+++ gv.syll.fe/src/routes/gv/(core)/+layout.svelte
@@ -43,17 +43,6 @@
 			<div class="flex items-center gap-2 px-4">
 				<Sidebar.Trigger class="-ml-1" />
 				<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
-				<!-- <Breadcrumb.Root>
-          <Breadcrumb.List>
-            <Breadcrumb.Item class="hidden md:block">
-              <Breadcrumb.Link href="##">Building Your Application</Breadcrumb.Link>
-            </Breadcrumb.Item>
-            <Breadcrumb.Separator class="hidden md:block" />
-            <Breadcrumb.Item>
-              <Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
-            </Breadcrumb.Item>
-          </Breadcrumb.List>
-        </Breadcrumb.Root> -->
 			</div>
 		</header>
 		<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
