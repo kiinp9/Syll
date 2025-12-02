82750328d19d1094af87f48b2f665f10242d1fdf
kiinp9 <vietcuong23122k2@gmail.com>
2025-12-02 15:31:39 +0700

    chore: update folder fe/be

---
+++ syll.be/syll.be.shared/Constants/Report/ReportConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace syll.be.shared.Constants.Report
+{
+    public static class ReportConstants
+    {
+        public const int TatCa = 1;
+        public const int DaCheck = 2;
+        public const int ChuaCheck = 3;
+        public const int ChuaImportData = 4;
+    }
+}
